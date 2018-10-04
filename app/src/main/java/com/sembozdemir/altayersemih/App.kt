e7934fffc4169747d5d60cdc4af0a9891a7c679a
Semih Bozdemir <sembozdemir@gmail.com>
2018-10-04 14:58:48 +0300

    Init Timber

--- app/src/main/java/com/sembozdemir/altayersemih/App.kt
+++ app/src/main/java/com/sembozdemir/altayersemih/App.kt
@@ -1,8 +1,10 @@
 package com.sembozdemir.altayersemih
 
+import android.util.Log
 import com.sembozdemir.altayersemih.core.injection.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import timber.log.Timber
 
 class App : DaggerApplication() {
 
@@ -12,5 +14,22 @@ class App : DaggerApplication() {
             .build()
             .also { it.inject(this) }
 
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(if (BuildConfig.DEBUG) Timber.DebugTree() else CrashReportingTree())
+    }
+
+    private inner class CrashReportingTree : Timber.Tree() {
+
+        override fun log(priority: Int, tag: String?, message: String,
+                         throwable: Throwable?) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return
+            }
+
+            // TODO: report warnings and errors by using a crash reporting tool
+        }
+    }
 
 }
